d11fe2b792bfa993c27a2603b1d9d28d2cbb08ec
Philip Jackson <p-jackson@live.com>
2024-06-28 00:33:02 +1200

    Remaps for split/window management

--- dot-config/my-nvim/lua/config/remap.lua
+++ dot-config/my-nvim/lua/config/remap.lua
@@ -1,7 +1,19 @@
+-- Show folder explorer (Netrw)
 vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
+
+-- Toggle undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+-- Keep cursor in the middle of the screen when scrolling and searching
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- Create and focus new split
+vim.keymap.set("n", "<leader>\\", ":vsplit<cr><C-w>w");
+vim.keymap.set("n", "<leader>-", ":split<cr><C-w>w");
+
+-- Close current window/split
+vim.keymap.set("n", "<leader>dd", "<C-w>q");
 
