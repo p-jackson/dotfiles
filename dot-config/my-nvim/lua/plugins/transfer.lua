481d38d3b332ebdc787802f86aeb21efe38a9935
Philip Jackson <p-jackson@live.com>
2024-08-19 15:58:51 +1200

    Move "transfer" plugin to my-nvim neovim config

---
+++ dot-config/my-nvim/lua/plugins/transfer.lua
@@ -0,0 +1,8 @@
+return {
+	{
+		"coffebar/transfer.nvim",
+		lazy = true,
+		cmd = { "TransferInit", "DiffRemote", "TransferUpload", "TransferDownload", "TransferDirDiff", "TransferRepeat" },
+		opts = {},
+	}
+}
