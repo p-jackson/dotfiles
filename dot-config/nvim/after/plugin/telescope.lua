eb17e83cb7fda4fa9ff2fd1b8c5cf6e0d37adc18
Philip Jackson <p-jackson@live.com>
2024-09-09 11:54:41 +1200

    Use telescope for browsing references
    Telescope seems to have out-of-the-box integration with lsp-zero

--- dot-config/nvim/after/plugin/telescope.lua
+++ dot-config/nvim/after/plugin/telescope.lua
@@ -3,4 +3,4 @@ vim.keymap.set("n", "•`_´•p", builtin.find_files, {}) -- Alacritty maps Cmd
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fc", builtin.grep_string, {}) -- Search all files for text under cursor
 vim.keymap.set("n", "<leader>rg", builtin.live_grep, {}) -- Fuzzy find in file contents
-
+vim.keymap.set("n", "gr", builtin.lsp_references, {})
