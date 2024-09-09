ed581ec21be748eda77648b873555f9dea14c517
Philip Jackson <p-jackson@live.com>
2024-09-09 21:51:44 +1200

    Speed up telescope in huge projects
    telescope-fzf-native is a native implementation of fzf so it will speed
    up search in very large codebases like wpcom and Jetpack.
    
    Also switched to the official release version of telescope rather than
    pinning to 0.1.x.

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,13 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
 	dependencies = {
-		"nvim-lua/plenary.nvim"
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		}
 	}
 }
-
