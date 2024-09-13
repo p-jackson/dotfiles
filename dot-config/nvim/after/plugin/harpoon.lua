0266a709629b9266d6570165b9e9124e911c7889
Philip Jackson <philip@Philips-MacBook-Pro.local>
2024-09-13 22:09:33 +1200

    Vertically re-centre after navigating in harpoon

--- dot-config/nvim/after/plugin/harpoon.lua
+++ dot-config/nvim/after/plugin/harpoon.lua
@@ -5,8 +5,8 @@ vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 -- Alacritty maps Cmd+J to •`_´•j etc.
-vim.keymap.set("n", "•`_´•j", function() ui.nav_file(1) end)
-vim.keymap.set("n", "•`_´•k", function() ui.nav_file(2) end)
-vim.keymap.set("n", "•`_´•l", function() ui.nav_file(3) end)
-vim.keymap.set("n", "•`_´•;", function() ui.nav_file(4) end)
+vim.keymap.set("n", "•`_´•j", function() ui.nav_file(1); vim.cmd("normal! zz"); end)
+vim.keymap.set("n", "•`_´•k", function() ui.nav_file(2); vim.cmd("normal! zz"); end)
+vim.keymap.set("n", "•`_´•l", function() ui.nav_file(3); vim.cmd("normal! zz"); end)
+vim.keymap.set("n", "•`_´•;", function() ui.nav_file(4); vim.cmd("normal! zz"); end)
 
