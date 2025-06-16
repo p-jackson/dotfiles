d630001e39732983a603ec029431529a0e9c59e5
Philip Jackson <p-jackson@live.com>
2025-06-16 21:49:06 +1200

    Update deprecated vim.highlight to vim.hl

--- dot-config/nvim/lua/config/highlight-on-yank.lua
+++ dot-config/nvim/lua/config/highlight-on-yank.lua
@@ -2,6 +2,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking text',
 	group = vim.api.nvim_create_augroup('custom-plugin-highlight-on-yank', { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 })
