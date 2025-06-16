300451d01ba0fc20e8e02aced997560691cd5d72
Philip Jackson <p-jackson@live.com>
2025-06-16 21:31:46 +1200

    Add avante for AI

---
+++ dot-config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,43 @@
+return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	lazy = false,
+	version = "*",
+	opts = {
+		provider = "claude",
+		claude = {
+			endpoint = "https://api.anthropic.com",
+			-- model = "claude-3-7-sonnet-20250219",
+			model = "claude-sonnet-4-20250514",
+			temperature = 0,
+			max_tokens = 4096,
+		},
+		behaviour = {
+			enable_cursor_planning_mode = true,
+			enable_claude_text_editor_tool_mode = true,
+		},
+		windows = {
+			ask = {
+				start_insert = false,
+			},
+		},
+	},
+	build = "make",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"echasnovski/mini.pick", -- for file_selector provider mini.pick
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			'MeanderingProgrammer/render-markdown.nvim',
+			enabled = false,
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
+}
